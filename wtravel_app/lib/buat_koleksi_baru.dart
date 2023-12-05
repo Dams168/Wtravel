346091b702cde80422691d3a4e8d19566d3b1e99
fardz <faridzazzarqi39@gmail.com>
2023-12-05 14:50:43 +0700

    membuat page buat koleksi baru

--- wtravel_app/lib/buat_koleksi_baru.dart
+++ wtravel_app/lib/buat_koleksi_baru.dart
@@ -1,7 +1,27 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class BuatKoleksiBaru extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class BuatKoleksiBaru extends StatefulWidget {
   const BuatKoleksiBaru({super.key});
+
+  @override
+  State<BuatKoleksiBaru> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<BuatKoleksiBaru> {
+  File? image;
+
+  Future getImage() async {
+    final ImagePicker picker = ImagePicker();
+
+    // Pick an image.
+    final XFile? imagePicked =
+        await picker.pickImage(source: ImageSource.gallery);
+    image = File(imagePicked!.path);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +45,131 @@ class BuatKoleksiBaru extends StatelessWidget {
                 color: Color(0xFF140C47),
               ),
             ),
+          ],
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 5),
+                  child: const Text(
+                    'Nama Koleksi',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                )
+              ],
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Nama Koleksi',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(color: Colors.white, width: 1),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 5),
+                  child: const Text(
+                    'Tanggal',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                )
+              ],
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Masukkan Tanggal',
+                prefixIcon: Icon(Icons.calendar_month_outlined),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(color: Colors.white, width: 1),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 5),
+                  child: const Text(
+                    'Tambah Cover',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 211,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5.0),
+                    color: const Color(0x50425884),
+                  ),
+                  child: image != null
+                      ? Container(
+                          height: 211,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: Image.file(
+                            image!,
+                            fit: BoxFit.cover,
+                          ))
+                      : Container(),
+                ),
+                const SizedBox(height: 15),
+                TextButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: const Color(0xFF140C47)),
+                  onPressed: () async {
+                    await getImage();
+                  },
+                  child: const Text(
+                    'Pilih Dari Galeri',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 195),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF140C47),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text(
+                      'Lanjutkan',
+                      style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
--- wtravel_app/lib/main.dart
+++ wtravel_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wtravel_app/perjalanan_simpan.dart';
+import 'package:wtravel_app/buat_koleksi_baru.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const PerjalananSimpan(),
+      home: const BuatKoleksiBaru(),
     );
   }
 }
