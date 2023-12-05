c9ff53683a5deec848bbdcf881ce34bf5d264039
Dams168 <adamrohman2003@gmail.com>
2023-12-05 23:01:11 +0700

    feat: add font Poppins

--- wtravel_app/lib/main.dart
+++ wtravel_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WTravel',
-      theme: ThemeData(),
+      theme: ThemeData(fontFamily: "Poppins"),
       home: const SplashScreen(),
     );
   }
