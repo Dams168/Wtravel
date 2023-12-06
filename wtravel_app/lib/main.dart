a2f4fe99305b3194169f5d56582c69e9cc4b007c
fardz <faridzazzarqi39@gmail.com>
2023-12-06 11:32:42 +0700

    membuat page tambah item

--- wtravel_app/lib/main.dart
+++ wtravel_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wtravel_app/buat_koleksi_baru.dart';
 import 'package:wtravel_app/tambah_item.dart';
 
 void main() {
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const BuatKoleksiBaru(),
+      home: const TambahItem(),
     );
   }
 }
--- wtravel_app/lib/tambah_item.dart
+++ wtravel_app/lib/tambah_item.dart
@@ -25,7 +25,7 @@ class TambahItem extends StatelessWidget {
           children: [
             Container(
               margin: const EdgeInsets.only(bottom: 10),
-              height: 90,
+              height: 100,
               width: MediaQuery.of(context).size.width,
               decoration: const BoxDecoration(
                   color: Color(0xFFFFFFFF),
@@ -33,13 +33,207 @@ class TambahItem extends StatelessWidget {
               child: Row(
                 children: [
                   Container(
-                    child: Image.asset('assets/'),
+                    padding: const EdgeInsets.all(5),
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(5)),
+                      child: Image.asset(
+                        'assets/images/tambah-item-gunung-bromo.jpg',
+                        height: MediaQuery.of(context).size.height,
+                        width: 120,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                  Column(),
-                  SizedBox()
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Row(
+                              children: [
+                                Text(
+                                  'Bromo',
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/icons/perjalanan-simpan-location.png',
+                                ),
+                                const SizedBox(width: 3),
+                                const Text(
+                                  'Jawa Timur',
+                                  style: TextStyle(
+                                      fontSize: 10, color: Color(0xFF140C47)),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Text(
+                                  'Rating :',
+                                  style: TextStyle(fontSize: 11),
+                                ),
+                                Image.asset(
+                                  'assets/icons/perjalanan-simpan-bintang.png',
+                                  height: 12,
+                                  width: 12,
+                                  fit: BoxFit.contain,
+                                ),
+                                const SizedBox(width: 3),
+                                const Text('4,5',
+                                    style: TextStyle(fontSize: 11)),
+                              ],
+                            ),
+                            const Row(
+                              children: [
+                                Text(
+                                  'Rp 300.000',
+                                  style: TextStyle(
+                                      color: Color(0xFF140C47), fontSize: 10),
+                                ),
+                                Text(
+                                  '/orang',
+                                  style: TextStyle(
+                                      color: Color(0xFFCACACA), fontSize: 10),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 100),
+                      SizedBox(
+                        child: Checkbox(value: true, onChanged: (value) {}),
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            )
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 10),
+              height: 100,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                  color: Color(0xFFFFFFFF),
+                  borderRadius: BorderRadius.all(Radius.circular(5))),
+              child: Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(5)),
+                      child: Image.asset(
+                        'assets/images/tambah-item-gunung-pulaukomodo.jpg',
+                        height: MediaQuery.of(context).size.height,
+                        width: 120,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Row(
+                              children: [
+                                Text(
+                                  'Pulau Komodo',
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/icons/perjalanan-simpan-location.png',
+                                ),
+                                const SizedBox(width: 3),
+                                const Text(
+                                  'Nusa Tenggara Timur',
+                                  style: TextStyle(
+                                      fontSize: 10, color: Color(0xFF140C47)),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Text(
+                                  'Rating :',
+                                  style: TextStyle(fontSize: 11),
+                                ),
+                                Image.asset(
+                                  'assets/icons/perjalanan-simpan-bintang.png',
+                                  height: 12,
+                                  width: 12,
+                                  fit: BoxFit.contain,
+                                ),
+                                const SizedBox(width: 3),
+                                const Text('4,7',
+                                    style: TextStyle(fontSize: 11)),
+                              ],
+                            ),
+                            const Row(
+                              children: [
+                                Text(
+                                  'Rp 150.000',
+                                  style: TextStyle(
+                                      color: Color(0xFF140C47), fontSize: 10),
+                                ),
+                                Text(
+                                  '/orang',
+                                  style: TextStyle(
+                                      color: Color(0xFFCACACA), fontSize: 10),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 73),
+                      SizedBox(
+                        child: Checkbox(value: true, onChanged: (value) {}),
+                      ),
+                      const SizedBox(height: 200),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFF140C47),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              child: const Text(
+                                'Lanjutkan',
+                                style: TextStyle(
+                                    color: Color(0xFFFFFFFF), fontSize: 20),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
