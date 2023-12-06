853ed8c14c79dc9b3824a3551f836536356041cf
fardz <faridzazzarqi39@gmail.com>
2023-12-06 14:29:59 +0700

    membuat page detail koleksi

--- wtravel_app/lib/main.dart
+++ wtravel_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wtravel_app/tambah_item.dart';
+import 'package:wtravel_app/detail_koleksi..dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const TambahItem(),
+      home: const DetailKoleksi(),
     );
   }
 }
